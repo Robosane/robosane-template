70c8b78f6b000ed06cd3b5656318cd507698e964
robobenklein <robobenklein@gmail.com>
2015-03-15 16:26:36 -0400

    Mobile-friendly fix.

--- index.php
+++ index.php
@@ -49,6 +49,7 @@ if ($this->countModules('position-7')) {
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <jdoc:include type="head" />
 
     <!-- Custom Fonts -->
